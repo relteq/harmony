39c0303014fa18dca1778f60b2252a2c59eea398
Oliver Newland <onewland@gmail.com>
2011-02-04 07:02:39 -0800

    Initial Relteq Redmine plugin commit

---
+++ vendor/plugins/redmine_relteq_hooks/init.rb
@@ -0,0 +1,9 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_relteq_hooks do
+  name 'Redmine Relteq Hooks plugin'
+  author 'Relteq Systems'
+  description 'Integrate Relteq models/data into Redmine project system'
+  version '0.0.1'
+  url 'http://relteqsystems.com/'
+end
