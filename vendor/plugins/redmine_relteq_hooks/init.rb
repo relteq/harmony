9759bb8a4472ff05283b4313348cbb42102ae597
Oliver Newland <onewland@gmail.com>
2011-02-04 07:36:53 -0800

    Sample permissions init data/controller/views for in-project menu and views

--- vendor/plugins/redmine_relteq_hooks/init.rb
+++ vendor/plugins/redmine_relteq_hooks/init.rb
@@ -3,7 +3,12 @@ require 'redmine'
 Redmine::Plugin.register :redmine_relteq_hooks do
   name 'Redmine Relteq Hooks plugin'
   author 'Relteq Systems'
-  description 'Integrate Relteq models/data into Redmine project system'
+  description 'Integrate Relteq UI into Redmine project system'
   version '0.0.1'
   url 'http://relteqsystems.com/'
+
+  permission :view_simulation_models, :scenarios => [:index, :show]
+  permission :edit_simulation_models, :scenarios => [:edit, :update, :destroy, :delete_all]
+  permission :create_simulation_models, :scenarios => [:new, :create]
+  menu :project_menu, :scenarios, {:controller => 'scenarios', :action => 'index'}, :caption => 'Models', :param => :project_id, :after => :overview
 end
