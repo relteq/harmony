8d3be659ce93bc32eb5b0c67d786d1e8f4e2a937
Oliver Newland <onewland@gmail.com>
2011-02-18 15:18:24 -0800

    Enforce boundaries between Scenarios of different projects permission-wise

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -1,6 +1,9 @@
 class ScenariosController < ApplicationController
   menu_item :configurations  
   before_filter :populate_menu
+  before_filter do |controller|
+    controller.authorize(:configurations)
+  end
   helper :sort
   include SortHelper
    
