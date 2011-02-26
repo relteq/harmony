0ada5ccbd9c1cd5e1be09055dbdc8f02267be2dc
Oliver Newland <onewland@gmail.com>
2011-02-25 18:34:17 -0800

    Add progress field to simulation hash

--- lib/simulation.rb
+++ lib/simulation.rb
@@ -4,7 +4,13 @@ class Simulation
   def self.all_for_user(user_id)
     begin
       response = self.http_get_batches(user_id)
-      response["batches"] if response["status"] == "ok"
+      if response["status"] == "ok"
+        response["batches"].map{|s| s[:progress] = s[:n_complete] / s[:n_runs]}
+        #response["batches"].map{|s| s[:progress] = rand(100)}
+        return response["batches"]
+      else
+        response = []
+      end
     rescue
       response = []
     end
