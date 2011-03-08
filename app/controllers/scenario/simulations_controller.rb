d68d9cbca0a474ca3b27f7461214903719cb1e1d
Oliver Newland <onewland@gmail.com>
2011-03-08 11:15:53 -0800

    Updated YAML sent to runweb to encapsulate simulation parameters inside a param object

--- app/controllers/scenario/simulations_controller.rb
+++ app/controllers/scenario/simulations_controller.rb
@@ -12,40 +12,40 @@ class Scenario::SimulationsController < ApplicationController
   end
 
   def create
-    options = {}
+    options = { :engine => 'dummy' }
+    options[:param] = {}
     # :simple being set means this was called from 'Run Simulation'
     # rather than 'Run Simulation Batch'
     if params[:simple]
       options[:name] = @scenario.name
       options[:n_runs] = 1
       options[:mode] = 'simulation'
-      options[:b_time] = 0.0
-      options[:duration] = 0.0
-      options[:control] = true
-      options[:qcontrol] = true
-      options[:events] = true
-      options[:engine] = 'dummy'
+      options[:param][:b_time] = 0.0
+      options[:param][:duration] = 0.0
+      options[:param][:control] = true
+      options[:param][:qcontrol] = true
+      options[:param][:events] = true
     else
       options[:name] = params[:name]
       options[:n_runs] = params[:n_runs]
       options[:mode] = params[:mode]
-      options[:b_time] = params[:begin_time_h].to_f + 
+      options[:param][:b_time] = params[:begin_time_h].to_f + 
                          params[:begin_time_m].to_f / 60.0 + 
                          params[:begin_time_s].to_f / 3600.0
       if params[:end_time_type] == 'duration'
-        options[:duration] = params[:end_time_h].to_f + 
+        options[:param][:duration] = params[:end_time_h].to_f + 
                            params[:end_time_m].to_f / 60.0 + 
                            params[:end_time_s].to_f / 3600.0
       elsif params[:end_time_type] == 'end_time'
-        options[:duration] = params[:end_time_h].to_f + 
+        options[:param][:duration] = params[:end_time_h].to_f + 
                            params[:end_time_m].to_f / 60.0 + 
                            params[:end_time_s].to_f / 3600.0 -
                            options[:b_time]
 
       end
-      options[:control] = !!params[:control]
-      options[:qcontrol] = !!params[:qcontrol]
-      options[:events] = !!params[:events]
+      options[:param][:control] = !!params[:control]
+      options[:param][:qcontrol] = !!params[:qcontrol]
+      options[:param][:events] = !!params[:events]
     end
     options[:user] = User.current.id
     Simulation.launch(options)
