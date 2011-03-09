11eb7de26d3a452a4f40de1e6442541837aadb6c
Oliver Newland <onewland@gmail.com>
2011-03-09 08:29:02 -0800

    Move js_callback_redirect from ApplicationHelper to ConfigurationsHelper

--- app/helpers/configurations_helper.rb
+++ app/helpers/configurations_helper.rb
@@ -1,2 +1,9 @@
 module ConfigurationsHelper
+  def js_callback_redirect(url_options)
+    %Q{
+      callback: function() {
+        window.location = "#{url_for(url_options)}";
+      }
+    }
+  end
 end
