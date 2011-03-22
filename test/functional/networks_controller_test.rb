0e4cf22f45eed5a8cece9d9abfc6783a190ddf3b
Oliver Newland <onewland@gmail.com>
2011-03-22 11:17:10 -0700

    Change NetworkControllerTest to NetworksControllerTest

---
+++ test/functional/networks_controller_test.rb
@@ -0,0 +1,8 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class NetworksControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
