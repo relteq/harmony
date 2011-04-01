2246e8bc80dd4e1c1209906e45e9a130e522f411
Oliver Newland <onewland@gmail.com>
2011-03-31 21:08:44 -0700

    Fixed typo in SimulationBatchReport model

--- app/models/simulation_batch_report.rb
+++ app/models/simulation_batch_report.rb
@@ -1,5 +1,5 @@
 class SimulationBatchReport < ActiveRecord::Base
-  has_many :simlulation_batch_lists
+  has_many :simulation_batch_lists
   
   belongs_to :scatter_plot
   belongs_to :color_pallette
