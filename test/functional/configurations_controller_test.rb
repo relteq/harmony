cb89fd8e1a2dadb4c117fc306daf3df23285f897
Oliver Newland <onewland@gmail.com>
2011-04-01 10:59:29 -0700

    Added tests for ConfigurationsController

--- test/functional/configurations_controller_test.rb
+++ test/functional/configurations_controller_test.rb
@@ -1,8 +1,32 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class ConfigurationsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "with authorized user" do
+    setup do 
+      @request = ActionController::TestRequest.new
+      @request.session[:user_id] = 1
+      @project = Project.generate!
+    end
+
+    context "get show" do
+      setup { get :show, :project_id => @project }
+
+      should "return success" do
+        assert_response :success
+      end
+    end
+  end
+
+  context "with unauthorized user" do
+    setup do 
+      @request = ActionController::TestRequest.new
+      @project = Project.generate!
+      @request.session[:user_id] = 7
+    end
+
+    should "return forbidden" do
+      get :show, :project_id => @project
+      assert_response 403
+    end
   end
 end
