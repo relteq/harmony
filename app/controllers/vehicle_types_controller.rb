ead056b6e46ba9fe596741e6b58393295fbf5e0b
Oliver Newland <onewland@gmail.com>
2011-04-05 11:24:09 -0700

    VehicleTypes controller functionality and some tests

---
+++ app/controllers/vehicle_types_controller.rb
@@ -0,0 +1,57 @@
+class VehicleTypesController < ConfigurationsApplicationController 
+  before_filter :require_scenario
+
+  def new
+    @vehicle_type = @scenario.vehicle_types.build
+  end
+
+  def create
+    @vehicle_type = @scenario.vehicle_types.create(params[:vehicle_type])
+
+    if @vehicle_type.save
+      respond_to do |format|
+        format.html do
+          flash[:notice] = 'Vehicle Type created successfully.'
+          redirect_to edit_project_configuration_scenario_path(@project, @scenario)
+        end
+        format.json { render :json => {:success => true, 
+                                       :display => @vehicle_type.short_display, 
+                                       :id => @vehicle_type.id}}
+      end
+    else
+      respond_to do |format|
+        format.html { render :new }
+        format.json { render :json => {:success => false,
+                                       :errors => @vehicle_type.errors}}
+      end
+    end
+  end
+
+  def destroy
+    @vehicle_type = @scenario.vehicle_types.find(params[:id])
+    if @scenario.vehicle_types.count > 1
+      @success = @vehicle_type.destroy
+    else
+      @error_message = 'Cannot delete only vehicle type.'
+    end
+    respond_to do |format|
+      format.html do 
+        flash[:error] = @error_message
+        redirect_to edit_project_configuration_scenario_path(@project, @scenario) 
+      end
+      format.json { render :json => {:success => @success, 
+                                     :error_message => @error_message}}
+    end
+  end
+
+private
+  def require_scenario
+    begin
+      @scenario = @project.scenarios.find(params[:scenario_id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to :action => :index, :project_id => @project
+      flash[:error] = 'Scenario not found.'
+      return false
+    end
+  end
+end
--- test/functional/vehicle_types_controller_test.rb
+++ test/functional/vehicle_types_controller_test.rb
@@ -1,8 +1,64 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class VehicleTypesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "with authorized user" do
+    setup do
+      @request = ActionController::TestRequest.new
+      @project = Project.generate!
+      @scenario = Scenario.generate!
+      @project.scenarios << @scenario
+      @request.session[:user_id] = 1
+    end
+
+    context "post create" do
+      context "with valid params" do
+        setup do
+          @params = { :project_id => @project.id, 
+                      :scenario_id => @scenario.id,
+                      :vehicle_type => {:name => 'Heavy', 
+                         :weight => 2.0} }
+        end
+
+        should "increase vehicle type count by 1" do
+          assert_difference('VehicleType.count') do
+            post :create, @params
+          end
+        end 
+      end
+    end
+
+    context "delete destroy" do
+      context "with valid params" do
+        setup do
+          @vehicle_type = VehicleType.generate!
+          @scenario.vehicle_types << @vehicle_type
+          @params = { :project_id => @project.id, 
+                      :scenario_id => @scenario.id,
+                      :id => @vehicle_type.id }
+        end
+        
+        should "reduce the vehicle type count by 1" do
+          assert_difference('VehicleType.count', -1) do
+            delete :destroy, @params
+          end
+        end
+      end
+
+      context "with only 1 vehicle type in scenario" do
+        setup do
+          @vehicle_type = VehicleType.generate!
+          @scenario.vehicle_types = [@vehicle_type]
+          @params = { :project_id => @project.id, 
+                      :scenario_id => @scenario.id,
+                      :id => @vehicle_type.id }
+        end
+
+        should "not change vehicle type count" do
+          assert_no_difference('VehicleType.count') do
+            delete :destroy, @params
+          end
+        end
+      end
+    end
   end
 end
