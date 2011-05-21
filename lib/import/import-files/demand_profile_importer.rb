368fb20d1f5212160e2e8eb37350f797562d3bf9
Oliver Newland <onewland@gmail.com>
2011-05-20 22:21:55 -0700

    Add knob parameter to demand profile importer

--- lib/import/import-files/demand_profile_importer.rb
+++ lib/import/import-files/demand_profile_importer.rb
@@ -13,6 +13,7 @@ module Aurora
     end
     
     def self.import_xml dp_xml, ctx, dp
+      dp.knob       = dp_xml["knob"].to_i || 1
       dp.dt         = Float(dp_xml["dt"])
       dp.start_time = Float(dp_xml["start_time"] || 0)
       dp.link_id    = ctx.get_link_id(dp_xml["link_id"])
