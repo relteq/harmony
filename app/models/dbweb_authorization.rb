1effaa728cb48b39e27fc22523294f22cdfd8a1b
Oliver Newland <onewland@gmail.com>
2011-06-08 15:06:26 -0700

    Force dbweb auth to make expiration time UTC

--- app/models/dbweb_authorization.rb
+++ app/models/dbweb_authorization.rb
@@ -3,7 +3,7 @@ class DbwebAuthorization < ActiveRecord::Base
     DbwebAuthorization.create!(
       :object_type => obj.class.to_s,
       :object_id => obj.id,
-      :expiration => Time.now + 5.minutes,
+      :expiration => Time.now.utc + 5.minutes,
       :access_token => ActiveSupport::SecureRandom.base64(50)
     )
   end
