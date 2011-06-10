92b17b4dbe8c5ee2d16647f4c43c4fb21e68ea46
Oliver Newland <onewland@gmail.com>
2011-06-10 09:50:20 -0700

    Coerce max file size to integral type in S3 SWF uploader

--- vendor/plugins/s3-swf-upload-plugin/lib/s3_swf_upload/s3_config.rb
+++ vendor/plugins/s3-swf-upload-plugin/lib/s3_swf_upload/s3_config.rb
@@ -13,7 +13,7 @@ module S3SwfUpload
       @@access_key_id     = ENV['AMAZON_ACCESS_KEY_ID'] || config[RAILS_ENV]['access_key_id']
       @@secret_access_key = ENV['AMAZON_SECRET_ACCESS_KEY'] || config[RAILS_ENV]['secret_access_key']
       @@bucket            = ENV['AMAZON_S3_SWF_UPLOAD_BUCKET'] || config[RAILS_ENV]['bucket_name']
-      @@max_file_size     = ENV['AMAZON_S3_SWF_MAX_FILE_SIZE'] || config[RAILS_ENV]['max_file_size']
+      @@max_file_size     = ENV['AMAZON_S3_SWF_MAX_FILE_SIZE'].to_i || config[RAILS_ENV]['max_file_size']
       @@acl               = ENV['AMAZON_S3_SWF_UPLOAD_ACL'] || config[RAILS_ENV]['acl'] || 'private'
       
       unless @@access_key_id && @@secret_access_key
