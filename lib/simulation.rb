6725703a5599b8046c347c6a024d1a0f1baab9bc
Oliver Newland <onewland@gmail.com>
2011-06-13 14:10:51 -0700

    Enhance error reporting, make RUNWEB_URL_BASE act like DBWEB_URL_BASE in environment

--- lib/simulation.rb
+++ lib/simulation.rb
@@ -17,10 +17,12 @@ module Simulation
           response["batches"].map{|s| s[:progress] = s[:n_complete] / s[:n_runs] if s[:n_runs] }
           return response["batches"]
         else
-          response = []
+          response = :failure 
         end
-      rescue
+      rescue Exception => e
         Rails.logger.error "Error loading Simulation data for user ID #{user_id}"
+        Rails.logger.error "error text: #{e.message}"
+        Rails.logger.error "backtrace: #{e.backtrace.inspect}"
         response = []
       end
     end
@@ -28,26 +30,31 @@ module Simulation
     def launch(simulation_spec)
       begin
         http_post_simulation(simulation_spec)
-      rescue
+      rescue Exception => e
         Rails.logger.error "Error launching simulation for user ID #{simulation_spec[:user]}"
+        Rails.logger.error "error text: #{e.message}"
+        Rails.logger.error "backtrace: #{e.backtrace.inspect}"
         return false
       end
     end
 
   private
-    DEFAULT_HOST = ENV['RUNWEB_HOST'] || 'localhost'
-    DEFAULT_PORT = ENV['RUNWEB_PORT'] || 8097 
-    
-    def http_get_batches(user, host = DEFAULT_HOST, port = DEFAULT_PORT)
-      YAML.load Net::HTTP.get(host, "/user/#{user}", port)
+    def http_get_batches(user)
+      YAML.load(
+        Net::HTTP.get URI.parse(ENV['RUNWEB_URL_BASE'] + "/user/#{user}")
+      )
     end
 
-    def http_get_batch(batch_id, host = DEFAULT_HOST, port = DEFAULT_PORT)
-      YAML.load Net::HTTP.get(host, "/batch/#{batch_id}", port)
+    def http_get_batch(batch_id)
+      YAML.load( 
+        Net::HTTP.get URI.parse(ENV['RUNWEB_URL_BASE'] + "/batch/#{batch_id}")
+      )
     end
 
-    def http_post_simulation(options, host = DEFAULT_HOST, port = DEFAULT_PORT)
-      res = Net::HTTP.start(host, port) do |http|
+    def http_post_simulation(options)
+      uri = URI.parse(ENV['RUNWEB_URL_BASE'])
+      host, port = uri.host, uri.port
+      Net::HTTP.start(host, port) do |http|
         http.post('/batch/new', options.to_yaml)
         Rails.logger.info options.to_yaml
       end
