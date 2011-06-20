a8ecbce6bf39f8730a7a49c61d3dec9036f9cb53
Oliver Newland <onewland@gmail.com>
2011-06-20 09:34:55 -0700

    Delete dependent objects upon network deletion

--- app/models/network.rb
+++ app/models/network.rb
@@ -6,27 +6,28 @@ class Network < ActiveRecord::Base
   
   belongs_to :project
 
-  has_many :controller_sets
-  has_many :split_ratio_profile_sets
-  has_many :capacity_profile_sets
-  has_many :demand_profile_sets
-  has_many :initial_condition_sets
-  has_many :event_sets
+  has_many :controller_sets, :dependent => :destroy
+  has_many :split_ratio_profile_sets, :dependent => :destroy
+  has_many :capacity_profile_sets, :dependent => :destroy
+  has_many :demand_profile_sets, :dependent => :destroy
+  has_many :initial_condition_sets, :dependent => :destroy
+  has_many :event_sets, :dependent => :destroy
+
   has_many :events
   has_many :controllers
 
-  has_many :scenarios
+  has_many :scenarios, :dependent => :destroy
   
   has_many :network_lists, :foreign_key => "network_id", :class_name => "NetworkList"
   has_many :children, :through => :network_lists  
 
   has_many :routes
-  has_many :nodes
+  has_many :nodes, :dependent => :destroy
   has_many :links
   has_many :sensors
   
   relteq_time_attr :dt
-  
+
   def remove_from_scenario
     @scen = Scenario.find_by_network_id(id)
     if(@scen != nil)
