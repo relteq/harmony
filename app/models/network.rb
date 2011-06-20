8afed9f175f95fc35bdf1beff53f127739d305a6
Oliver Newland <onewland@gmail.com>
2011-06-20 09:58:00 -0700

    Another shot at network deletion

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,6 +1,6 @@
 class Network < ActiveRecord::Base
   include RelteqTime::ActiveRecordMethods
-  before_destroy :destroy_route_links
+  before_destroy :destroy_dependents_ordered
 
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :project_id
@@ -22,16 +22,19 @@ class Network < ActiveRecord::Base
   has_many :network_lists, :foreign_key => "network_id", :class_name => "NetworkList"
   has_many :children, :through => :network_lists
 
-  has_many :route_links, :dependent => :destroy
-  has_many :routes, :dependent => :destroy
-  has_many :nodes, :dependent => :destroy
-  has_many :links, :dependent => :destroy
-  has_many :sensors, :dependent => :destroy
+  has_many :routes
+  has_many :nodes
+  has_many :links
+  has_many :sensors
   
   relteq_time_attr :dt
 
-  def destroy_route_links 
+  def destroy_dependents_ordered 
     ActiveRecord::Base.connection.execute "DELETE FROM route_links WHERE network_id=#{self.id}"
+    Route.delete(self.route_ids)
+    Link.delete(self.link_ids)
+    Sensor.delete(self.sensor_ids)
+    Node.delete(self.node_ids)
   end
 
   def remove_from_scenario
