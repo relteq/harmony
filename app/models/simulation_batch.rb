bff6dd90bcb2dee56a8bb40652840a2c50292360
Oliver Newland <onewland@gmail.com>
2011-06-27 09:08:44 -0700

    Added complete/incomplete named scopes for simulation batch

--- app/models/simulation_batch.rb
+++ app/models/simulation_batch.rb
@@ -8,6 +8,6 @@ class SimulationBatch < ActiveRecord::Base
   has_many :reported_batches
   has_many :simulation_batch_lists, :through => :reported_batches
   
-  
-    
+  named_scope :incomplete, :conditions => ['percent_complete < 1']
+  named_scope :complete, :conditions => ['percent_complete = 1']
 end
