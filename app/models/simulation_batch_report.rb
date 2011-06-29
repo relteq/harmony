6e7347b6d6a33c579b071e6790173ad1ddc0ebd3
Oliver Newland <onewland@gmail.com>
2011-06-29 09:01:30 -0700

    Code reformat

--- app/models/simulation_batch_report.rb
+++ app/models/simulation_batch_report.rb
@@ -64,59 +64,57 @@ class SimulationBatchReport < ActiveRecord::Base
   end
   
   def to_xml
-      builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
-        xml.guidata{
-          xml.cmb_export "PDF"
-          xml.report_id self.id
-          xml.cmb_reporttype self.report_type
-          xml.txt_congspeed self.congestion_speed
-          xml.txt_maxpointspercurve self.max_data_points
-          xml.cbx_orperf_time self.or_perf_t
-          xml.cbx_orperf_space self.or_perf_s
-          xml.cbx_orperf_contour self.or_perf_c
-          xml.cbx_routeperf_time self.route_perf_t
-          xml.cbx_routeperf_space self.route_perf_s
-          xml.cbx_routeperf_contour self.route_perf_c
-          xml.cbx_routetraveltime self.route_tt_t
-          xml.cbx_routetrajectories self.route_tt_c
-          xml.cbx_dolegend self.legend
-          xml.cbx_dofill self.fill_plots
-          xml.cbx_linkstate self.link_perf
-          xml.cbx_sysperf self.network_perf
-          xml.outputfile ""
-          xml.txt_timefrom self.b_time
-          xml.txt_timeto self.duration
-          xml.txt_customxaxis ""
-          xml.cbx_boxplot false
-          xml.colors self.colors
-          xml.tbl_groups ""
-          scen_names = ""
-          xml.tbl_scenariogroups{
-            self.simulation_batch_list.simulation_batches.each do |sb|
-              xml.entry(:scenario => sb.scenario.name, :group => "")
-              scen_names += sb.scenario.name + ","      
-            end
-  
-          }
-          
-          xml.cmb_xaxis_subnetwork(:selected => "0") {"Network,Onramps,Mainline"}
-        	xml.cmb_xaxis_quantity(:selected => "0") {"Vehicle hours,Vehicle miles,Delay"}
-        	xml.cmb_yaxis_subnetwork(:selected => "0") {"Network,Onramps,Mainline"}
-        	xml.cmb_yaxis_quantity(:selected => "0") {"Vehicle hours,Vehicle miles,Delay"}
-        	xml.scenarios_old scen_names[0,scen_names.length-1]
-        	xml.datafiles_old ""
-        	xml.BatchList {
-        	  self.simulation_batch_list.simulation_batches.each do |sb|
-              xml.batch(:name => sb.scenario.name){
-                sb.output_files.each do |f|
-                  xml.data_file(:url => f.url)
-                end
-              }
-            end
-        	}
-        	
+    builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+      xml.guidata {
+        xml.cmb_export "PDF"
+        xml.report_id self.id
+        xml.cmb_reporttype self.report_type
+        xml.txt_congspeed self.congestion_speed
+        xml.txt_maxpointspercurve self.max_data_points
+        xml.cbx_orperf_time self.or_perf_t
+        xml.cbx_orperf_space self.or_perf_s
+        xml.cbx_orperf_contour self.or_perf_c
+        xml.cbx_routeperf_time self.route_perf_t
+        xml.cbx_routeperf_space self.route_perf_s
+        xml.cbx_routeperf_contour self.route_perf_c
+        xml.cbx_routetraveltime self.route_tt_t
+        xml.cbx_routetrajectories self.route_tt_c
+        xml.cbx_dolegend self.legend
+        xml.cbx_dofill self.fill_plots
+        xml.cbx_linkstate self.link_perf
+        xml.cbx_sysperf self.network_perf
+        xml.outputfile ""
+        xml.txt_timefrom self.b_time
+        xml.txt_timeto self.duration
+        xml.txt_customxaxis ""
+        xml.cbx_boxplot false
+        xml.colors self.colors
+        xml.tbl_groups ""
+        scen_names = ""
+        xml.tbl_scenariogroups{
+          self.simulation_batch_list.simulation_batches.each do |sb|
+            xml.entry(:scenario => sb.scenario.name, :group => "")
+            scen_names += sb.scenario.name + ","      
+          end
+
         }
-     
+        
+        xml.cmb_xaxis_subnetwork(:selected => "0") {"Network,Onramps,Mainline"}
+        xml.cmb_xaxis_quantity(:selected => "0") {"Vehicle hours,Vehicle miles,Delay"}
+        xml.cmb_yaxis_subnetwork(:selected => "0") {"Network,Onramps,Mainline"}
+        xml.cmb_yaxis_quantity(:selected => "0") {"Vehicle hours,Vehicle miles,Delay"}
+        xml.scenarios_old scen_names[0,scen_names.length-1]
+        xml.datafiles_old ""
+        xml.BatchList {
+          self.simulation_batch_list.simulation_batches.each do |sb|
+            xml.batch(:name => sb.scenario.name){
+              sb.output_files.each do |f|
+                xml.data_file(:url => f.url)
+              end
+            }
+          end
+        }
+      }
     end
     builder.to_xml
   end
