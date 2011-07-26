fc3bde6ed68dddf9fcee020e8dc1f622d72aaedc
Oliver Newland <onewland@gmail.com>
2011-07-26 11:15:12 -0700

    Initialize S3 with app to generate private S3 URLs for secure user downloads

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -18,3 +18,10 @@ if File.exists?(filename)
     raise "Error connecting to S3, export will fail." unless Rails.env == 'test'
   end
 end
+
+if ENV['AMAZON_ACCESS_KEY_ID'] && ENV['AMAZON_SECRET_ACCESS_KEY']
+  AWS::S3::Base.establish_connection!(
+    :access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
+  )
+end
