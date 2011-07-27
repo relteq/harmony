07d6ff7bf94eb8fc4144b3f7b3695b6b378f5fbb
Oliver Newland <onewland@gmail.com>
2011-07-27 13:34:55 -0700

    Reformat SimulationBatchController

--- app/controllers/simulation_batches_controller.rb
+++ app/controllers/simulation_batches_controller.rb
@@ -3,72 +3,72 @@ class SimulationBatchesController < ApplicationController
   include SortHelper
   
   def index
-      sort_init 'name', 'asc'
-      sort_update %w(name created_at user_id_creator)
+    sort_init 'name', 'asc'
+    sort_update %w(name created_at user_id_creator)
 
-      case params[:format]
-      when 'xml', 'json'
-        @offset, @limit = api_offset_and_limit      
-      else
-        @limit = per_page_option
-      end
-     
-     
-      begin
-        @project = Project.find(params[:project_id])
-      rescue ActiveRecord::RecordNotFound
-        render :file => "#{Rails.root}/public/404.html", :status => 404
-        return false
-      end
- 
-      @item_count = SimulationBatch.count( :conditions => {:scenario_id => @project.scenarios,
-                                                           :percent_complete => 1,
-                                                           :succeeded => true});
-      @item_pages = Paginator.new self, @item_count, @limit, params['page']
-      @offset ||= @item_pages.current.offset
-      @items_show = SimulationBatch.find  :all,
-                                :conditions => {:scenario_id => @project.scenarios, 
-                                                :percent_complete => 1,
-                                                :succeeded => true},
-                                :order => sort_clause,
-                                :limit  =>  @limit,
-                                :offset =>  @offset
+    case params[:format]
+    when 'xml', 'json'
+      @offset, @limit = api_offset_and_limit      
+    else
+      @limit = per_page_option
+    end
+   
+    begin
+      @project = Project.find(params[:project_id])
+    rescue ActiveRecord::RecordNotFound
+      render :file => "#{Rails.root}/public/404.html", :status => 404
+      return false
+    end
 
-      #sets up objects for report_generator
-      @simulation_report = SimulationBatchReport.new
-      @simulation_report.default_report_settings!
-      
-      respond_to do |format|
-        format.html { render :layout => !request.xhr? } # index.html.erb
-        format.xml  { render :xml =>  @items_show}
-      end
+    @item_count = SimulationBatch.count(:conditions => {
+      :scenario_id => @project.scenarios,
+      :percent_complete => 1,
+      :succeeded => true
+    })
+
+    @item_pages = Paginator.new self, @item_count, @limit, params['page']
+    @offset ||= @item_pages.current.offset
+    @items_show = SimulationBatch.find  :all,
+                              :conditions => {:scenario_id => @project.scenarios, 
+                                              :percent_complete => 1,
+                                              :succeeded => true},
+                              :order => sort_clause,
+                              :limit  =>  @limit,
+                              :offset =>  @offset
+
+    #sets up objects for report_generator
+    @simulation_report = SimulationBatchReport.new
+    @simulation_report.default_report_settings!
+    
+    respond_to do |format|
+      format.html { render :layout => !request.xhr? } # index.html.erb
+      format.xml  { render :xml =>  @items_show}
+    end
   end
   
   def process_choices
-   
-      begin
-        @project = Project.find(params[:project_id])
-      rescue ActiveRecord::RecordNotFound
-        render :file => "#{Rails.root}/public/404.html", :status => 404
-        return false
-      end
- 
-      redir_path =  get_simulation_batch_action(params[:simulation_batch_action],params[:sim_ids])
+    begin
+      @project = Project.find(params[:project_id])
+    rescue ActiveRecord::RecordNotFound
+      render :file => "#{Rails.root}/public/404.html", :status => 404
+      return false
+    end
 
-      respond_to do |format|
-        if(redir_path != '' && (params[:sim_ids] != nil))
-            #render redir_path
-         format.html { redirect_to  redir_path}
-        else
-          if(redir_path == '')
-            flash[:error] = "There was a problem processing your action, please try again."
-          else 
-            flash[:error] = "Please check a simulation batch below before requesting an action."
-          end
-          format.html { redirect_to  project_simulation_batches_path(@project) } # index.html.erb         
+    redir_path =  get_simulation_batch_action(params[:simulation_batch_action],params[:sim_ids])
+
+    respond_to do |format|
+      if(redir_path != '' && (params[:sim_ids] != nil))
+        #render redir_path
+        format.html { redirect_to redir_path }
+      else
+        if(redir_path == '')
+          flash[:error] = "There was a problem processing your action, please try again."
+        else 
+          flash[:error] = "Please check a simulation batch below before requesting an action."
         end
+        format.html { redirect_to project_simulation_batches_path(@project) } # index.html.erb         
       end
-    
+    end
   end
   
   def delete_report
