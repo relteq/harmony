2f9968b88ffe5dd018d58ab4677899e3e7ec1439
Sean Morris <sean.smorris@gmail.com>
2011-07-28 15:05:35 -0700

    Measurement Data: Save/Rename

--- app/controllers/measurement_data_controller.rb
+++ app/controllers/measurement_data_controller.rb
@@ -2,6 +2,8 @@ class MeasurementDataController < ApplicationController
   helper :sort
   include SortHelper
   
+  before_filter :require_project, :only => [:index,:create]
+    
   def index
    
     sort_init 'name', 'asc'
@@ -14,12 +16,6 @@ class MeasurementDataController < ApplicationController
       @limit = per_page_option
     end
    
-    begin
-      @project = Project.find(params[:project_id])
-    rescue ActiveRecord::RecordNotFound
-      render :file => "#{Rails.root}/public/404.html", :status => 404
-      return false
-    end
 
     @item_count = @project.measurement_data.count
 
@@ -40,6 +36,67 @@ class MeasurementDataController < ApplicationController
       format.xml  { render :xml =>  @items_show}
     end
   end
+  
+  def create
+    begin
+      @mdat = MeasurementDatum.new(params[:measurement_data])
+      @mdat.url = 'sadf'
+      @mdat.project_id = @project.id
+      @mdat.user_id_creator = User.current.id
+    rescue
+      flash[:error] = l(:measurement_data_not_successful_upload)    
+    end 
+    
+    if(@mdat.save)
+        flash[:notice] = l(:measurement_data_successful_upload)
+    else
+        flash[:error] = l(:measurement_data_not_successful_upload)        
+    end
+    
+    respond_to do |format|
+      format.html { redirect_to project_measurement_data_path(@project) }
+    end
+  end
+  
+  def delete_file
+    begin
+      mdat =  MeasurementDatum.find(params[:id])
+      mdat.destroy
+      flash[:notice] = l(:measurement_datum) + ", '" + mdat.name + "', " + l(:label_success_delete)  
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = l(:measurement_datum_not_found)
+    rescue
+      flash[:error] = l(:measurement_datum_delete_not_success)
+    end
 
+    respond_to do |format|
+      format.html { redirect_to project_measurement_data_path(params[:project_id]) }
+    end 
+  end
 
+  def rename
+    begin
+       MeasurementDatum.save_rename(params[:measurement_data][:id],params[:measurement_data][:name])
+      flash[:notice] = l(:notice_successful_update) 
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = l(:measurement_datum_not_found)
+    rescue
+      flash[:error] = l(:notice_update_not_successful)
+    end
+    
+    respond_to do |format|
+     format.html {  redirect_to project_measurement_data_path(params[:project_id])  } # index.html.erb     
+    end
+  end
+  
+  private
+    def require_project
+      begin
+        @project = Project.find(params[:project_id])
+      rescue ActiveRecord::RecordNotFound
+        render :file => "#{Rails.root}/public/404.html", :status => 404
+        return false
+      end
+    end
+    
 end
--- app/models/measurement_datum.rb
+++ app/models/measurement_datum.rb
@@ -1,4 +1,24 @@
 class MeasurementDatum < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :data_type
+  validates_presence_of :data_format
+  validates_presence_of :url
   
   belongs_to :project
+  
+  
+  def creator
+    begin
+      u = User.find(user_id_creator)
+      u.name
+    rescue ActiveRecord::RecordNotFound
+      ''
+    end
+  end
+  
+  def self.save_rename(id,name) 
+    sim_batch =  MeasurementDatum.find(id)
+    sim_batch.name = name
+    sim_batch.save!
+  end
 end
