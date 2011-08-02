78a51b33c2dd030d10d105ddddcae4c1f6ede583
Oliver Newland <onewland@gmail.com>
2011-08-02 10:23:48 -0700

    Initialize Aurora worker user/group custom fields in migration

---
+++ db/migrate/20110802172016_initialize_aurora_worker_user_group_custom_fields.rb
@@ -0,0 +1,19 @@
+class InitializeAuroraWorkerUserGroupCustomFields < ActiveRecord::Migration
+  def self.up
+    UserCustomField.create!(
+      :name => 'aurora_worker_user', 
+      :field_format => 'string',
+      :default_value => ENV['AURORA_WORKER_USER']
+    )
+    UserCustomField.create!(
+      :name => 'aurora_worker_group', 
+      :field_format => 'string',
+      :default_value => ENV['AURORA_WORKER_GROUP']
+    )
+  end
+
+  def self.down
+    UserCustomField.find_by_name('aurora_worker_user').destroy
+    UserCustomField.find_by_name('aurora_worker_group').destroy
+  end
+end
