81d1d0a74b885b477421a894df45d500b09572ba
Sean Morris <sean.smorris@gmail.com>
2011-08-05 13:39:56 -0700

    Split Ratio Profiles: strip xml from profile table

--- app/helpers/split_ratio_profile_sets_helper.rb
+++ app/helpers/split_ratio_profile_sets_helper.rb
@@ -1,2 +1,9 @@
 module SplitRatioProfileSetsHelper
+  def strip_xml(profile)
+    return profile.split("<\/srm>")
+  end
+
+  def strip_xml_entry(r,index)
+    r.gsub("<srm>", (index+1).to_s + ") ").strip.insert(-1,"\n")
+  end
 end
