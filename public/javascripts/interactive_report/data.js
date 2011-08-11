94430d5f1839f1175536db2101256be8a8c93220
Oliver Newland <onewland@gmail.com>
2011-08-11 16:24:57 -0700

    Use 3D data from first contour plot in report

--- public/javascripts/interactive_report/data.js
+++ public/javascripts/interactive_report/data.js
@@ -1,12 +1,62 @@
-ReportViewerData = {
+ReportViewer.Data = {
   the_data: "",
+  x_data: [],
+  y_data: [],
+  z_data: [],
+
+  getContours : function() {
+    return ReportViewer.Data.the_data.find('plot[zlabel]');
+  },
+
+  getOneContour : function() {
+    return ReportViewer.Data.getContours().first();
+  },
+
+  normalizeArray : function(arr) {
+    var maxArr = Math.max.apply(null, arr);
+    for(var i = 0; i < arr.length; i++) {
+      arr[i] /= maxArr;
+      if(arr[i] > 1 || arr[i] < 0) {
+        console.log("Bad condition for arr[i], i = " 
+                    + i + ", arr[i] = " + arr[i]);
+      }
+    }
+    return arr;
+  },
+
+  loadXYZ : function(contour) {
+    var xdata = contour.find('xdata').text().split(/[,;]/),
+        ydata = contour.find('ydata').text().split(/[,;]/),
+        zdata = contour.find('zdata').text().split(/[,;]/);
+    var xl = xdata.length;
+    var yl = ydata.length;
+    var zl = zdata.length;
+    var minZ = Math.min.apply(null, zdata);
+    var maxZ = Math.max.apply(null, zdata);
+    console.log("minZ = " + minZ);
+    console.log("maxZ = " + maxZ);
+    console.log(xl);
+    console.log(yl);
+    console.log(zl);
+    zdata = ReportViewer.Data.normalizeArray(zdata);
+    return [xdata,ydata,zdata];
+  },
 
   xmlLoad: function(url) {
     $.ajax({
       url: report_url,
       dataType: 'jsonp',
       success: function(data) {
-        ReportViewerData['the_data'] = $.parseXML(data);
+        ReportViewer.Data.the_data = $($.parseXML(data.xml));
+        console.log(ReportViewer.Data.the_data);
+        var contour = ReportViewer.Data.getOneContour();
+        console.log(contour);
+        var axes_data = ReportViewer.Data.loadXYZ(contour);
+        ReportViewer.UI.populateGraphs(
+          axes_data[0], 
+          axes_data[1], 
+          axes_data[2]
+        );
       }
     });
   }
--- public/javascripts/interactive_report/ui.js
+++ public/javascripts/interactive_report/ui.js
@@ -1,65 +1,76 @@
-$(function() {
-  ReportViewerData.xmlLoad(report_url);
-  $("#x-axis-slider").slider();
-  $("#y-axis-slider").slider({ 
-    orientation: 'vertical' 
-  });
-  var latlng = new google.maps.LatLng(37.795467, -122.400341);
-  var myOptions = {
-    zoom: 16,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+ReportViewer.UI = {
+  z_data: {},
 
-  var polyOptions = {
-    strokeColor: '#000000',
-    strokeOpacity: 0.5,
-    strokeWeight: 3
-  };
+  populateGraphs: function(xdata,ydata,zdata) {
+    ReportViewer.UI.z_data = zdata;
 
-  var map = new google.maps.Map(
-    document.getElementById("map-view"),
-    myOptions
-  );
-  
-  function floatToColor(f)
-  {
-    return 'hsb( ' + f*100/359.0 + ', .78, .93)';
-  }
-  
-  var raphaels = {
-    r1: Raphael("three-d-graph", 950, 450)
-  };
-  var x = new Array(22500);
-  var rects = new Array(22500);
-  for(var i = 0; i < 22500; i++) {     
-     x[i] = Math.random();
-  }
-
-  for(var row = 0; row < 150; row++) {
-    for(var col = 0; col < 150; col++) {
-      var i = (row * 150) + col;
-      var rect = raphaels['r1'].rect(4*col,3*row,4,3);
-      rect.blockx = col;
-      rect.blocky = row;
-      rect.intensity = x[i];
-      var color = floatToColor(rect.intensity);
-      rect.attr('fill', color);
-      rect.attr('stroke', color);
-      rect.click(function(event) {
-        $("#debug-log").append("(" + this.blockx + ", "
-                             + this.blocky + ") = "
-                             + Math.floor(this.intensity*100)/100 + "; ");
-
-      });
-      rect.hover(function(event) {
-        this.toFront();
-        this.scale(5,5);
-      }, function(event) {
-        this.scale(1,1);
-      });
-      rects[i] = rect;
+    function floatToColor(f)
+    {
+      return 'hsb( ' + f.toFixed(4)*100.0/359.0 + ', .78, .93)';
     }
-  }
 
+    var raphaels = {
+      r1: Raphael("three-d-graph", 950, 450)
+    };
+    var xl = xdata.length;
+    var yl = ydata.length;
+    var zl = zdata.length;
+    console.log(xl,yl,zl);
+    var rects = new Array(zl);
+
+    for(var row = 0; row < yl; row++) {
+      for(var col = 0; col < xl; col++) {
+        var i = (row * xl) + col;
+        var rect = raphaels['r1'].rect(6*col,3*row,6,3);
+        rect.blockx = col;
+        rect.blocky = row;
+        rect.intensity = zdata[i];
+        var color = floatToColor(rect.intensity);
+        rect.attr('fill', color);
+        rect.attr('stroke', color);
+        rect.click(function(event) {
+          $("#debug-log").append("(" + this.blockx + ", "
+                               + this.blocky + ") = "
+                               + this.intensity + "; ");
+
+        });
+/*        rect.hover(function(event) {
+          this.toFront();
+          this.scale(5,5);
+        }, function(event) {
+          this.scale(1,1);
+        });*/
+        rects[i] = rect;
+      }
+    }
+  },
+
+  initialize: function() {
+    $("#x-axis-slider").slider();
+    $("#y-axis-slider").slider({ 
+      orientation: 'vertical' 
+    });
+    var latlng = new google.maps.LatLng(37.795467, -122.400341);
+    var myOptions = {
+      zoom: 16,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var polyOptions = {
+      strokeColor: '#000000',
+      strokeOpacity: 0.5,
+      strokeWeight: 3
+    };
+
+    var map = new google.maps.Map(
+      document.getElementById("map-view"),
+      myOptions
+    );
+  }
+}
+
+$(function() {
+  ReportViewer.Data.xmlLoad(report_url);
+  ReportViewer.UI.initialize();
 });
