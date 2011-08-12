a6461dd271e995eafc6c17c7859f960113b21614
Oliver Newland <onewland@gmail.com>
2011-08-12 12:26:30 -0700

    Dynamically resize scrollbars to data in 3D plot

--- public/javascripts/interactive_report/ui.js
+++ public/javascripts/interactive_report/ui.js
@@ -1,27 +1,62 @@
 ReportViewer.UI = {
+  plot_rect_size:  {
+    width: 6,
+    height: 4
+  },
+
+  debug: true,
   z_data: {},
+  crosshairs: {x: 0, y: 0},
+
+  adjustX: function(event, ui) {
+    if(ReportViewer.UI.debug) {
+      $("#crosshair_x").text(ui.value || 0);
+    }
+  },
+
+  adjustY: function(event, ui) {
+    if(ReportViewer.UI.debug) {
+      $("#crosshair_y").text(ui.value || 0);
+    }
+  },
 
   populateGraphs: function(xdata,ydata,zdata) {
-    ReportViewer.UI.z_data = zdata;
+    console.log(this);
+    this.z_data = zdata;
 
     function floatToColor(f)
     {
       return 'hsb( ' + f.toFixed(4)*100.0/359.0 + ', .78, .93)';
     }
 
-    var raphaels = {
-      r1: Raphael("three-d-graph", 950, 450)
-    };
     var xl = xdata.length;
     var yl = ydata.length;
     var zl = zdata.length;
+
+    $("#x-axis-slider").slider("option", "max", xl - 1);
+    $("#y-axis-slider").slider("option", "max", yl - 1);
+
+    var total_width = this.plot_rect_size.width * xl;
+    var total_height = this.plot_rect_size.height * yl;
+    
+    $("#x-axis-slider").css("width", total_width + "px");
+    $("#y-axis-slider").css("height", total_height + "px");
+
+    var raphaels = {
+      r1: Raphael("three-d-graph", total_width, total_height)
+    };
     console.log(xl,yl,zl);
     var rects = new Array(zl);
 
     for(var row = 0; row < yl; row++) {
       for(var col = 0; col < xl; col++) {
         var i = (row * xl) + col;
-        var rect = raphaels['r1'].rect(6*col,3*row,6,3);
+        var rect = raphaels['r1'].rect(
+          this.plot_rect_size.width*col,
+          this.plot_rect_size.height*row,
+          this.plot_rect_size.width,
+          this.plot_rect_size.height
+        );
         rect.blockx = col;
         rect.blocky = row;
         rect.intensity = zdata[i];
@@ -34,21 +69,20 @@ ReportViewer.UI = {
                                + this.intensity + "; ");
 
         });
-/*        rect.hover(function(event) {
-          this.toFront();
-          this.scale(5,5);
-        }, function(event) {
-          this.scale(1,1);
-        });*/
         rects[i] = rect;
       }
     }
+
+    $.plot("#xz-chart", [[[0,0],[1,1],[2,0.5]]]);
   },
 
   initialize: function() {
-    $("#x-axis-slider").slider();
-    $("#y-axis-slider").slider({ 
-      orientation: 'vertical' 
+    $("#x-axis-slider").slider({
+      slide: this.adjustX,
+    });
+    $("#y-axis-slider").slider({
+      slide: this.adjustY,
+      orientation: 'vertical'
     });
     var latlng = new google.maps.LatLng(37.795467, -122.400341);
     var myOptions = {
